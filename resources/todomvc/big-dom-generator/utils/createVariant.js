53a2f11535eacb663fa5e8ac5d4bf7de35b11fc4
Issack John <issackjohn@microsoft.com>
2023-12-15 15:05:55 -0800

    extract writing to file

--- resources/todomvc/big-dom-generator/utils/createVariant.js
+++ resources/todomvc/big-dom-generator/utils/createVariant.js
@@ -33,7 +33,6 @@ function vary(cssSelector, propValueTuples, operation) {
         });
 
         root = root.toString();
-        fs.writeFileSync(OUTPUT_FILE_PATH, root.toString());
     } catch (error) {
         console.error("An error occurred while varying the CSS:", error);
     }
@@ -43,6 +42,7 @@ try {
     const css = fs.readFileSync(INPUT_FILE_PATH, "utf-8");
     root = postcss.parse(css);
     vary(".tree-area", [["isolation", "isolate"]], "add");
+    fs.writeFileSync(OUTPUT_FILE_PATH, root.toString());
 } catch (error) {
     console.error("An error occurred while reading the CSS file:", error);
 }
