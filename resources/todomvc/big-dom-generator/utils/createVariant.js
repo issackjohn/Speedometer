76223624e68fcbc8b62c72d50cc64325bc2c96b9
Issack John <issackjohn@microsoft.com>
2023-12-19 11:01:46 -0800

    use dictionary instead

--- resources/todomvc/big-dom-generator/utils/createVariant.js
+++ resources/todomvc/big-dom-generator/utils/createVariant.js
@@ -7,19 +7,19 @@ const OUTPUT_FILE_PATH = "./dist/big-dom-generator-v2.css";
 let root;
 
 /**
- * Modifies CSS rules based on the provided selector, tuple, and operation.
+ * Modifies CSS rules based on the provided selector, dictionary, and operation.
  *
  * @param {string} cssSelector - The CSS selector to match.
- * @param {Array<[string, string]>} propValueTuples - The property-value tuples to apply.
+ * @param {Object} propValueDict - The property-value dictionary to apply.
  * @param {string} operation - The operation to perform ('add', 'remove', or 'modify'). At present, only the "add" operation is implemented.
  */
-function vary(cssSelector, propValueTuples, operation) {
+function vary(cssSelector, propValueDict, operation) {
     try {
         root.walkRules((rule) => {
             const selector = selectorParser().processSync(rule);
 
             if (selector === cssSelector) {
-                const props = propValueTuples.map(([prop, value]) => ({ prop, value }));
+                const props = Object.entries(propValueDict).map(([prop, value]) => ({ prop, value }));
 
                 switch (operation) {
                     case "add":
@@ -38,7 +38,7 @@ function vary(cssSelector, propValueTuples, operation) {
 try {
     const css = fs.readFileSync(INPUT_FILE_PATH, "utf-8");
     root = postcss.parse(css);
-    vary(".tree-area", [["isolation", "isolate"]], "add");
+    vary(".tree-area", { isolation: "isolate" }, "add");
     fs.writeFileSync(OUTPUT_FILE_PATH, root.toString());
 } catch (error) {
     console.error("An error occurred while processing the CSS:", error);
