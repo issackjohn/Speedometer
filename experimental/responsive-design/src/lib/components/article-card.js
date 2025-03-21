72c4db5f2abd73908b33638a7a10c4996cec5ede
Issack John <issackjohn@microsoft.com>
2025-03-21 13:41:48 -0700

    Refactor ArticleCard component layout; adjust spacing and improve tag presentation

--- experimental/responsive-design/src/lib/components/article-card.js
+++ experimental/responsive-design/src/lib/components/article-card.js
@@ -24,15 +24,15 @@ class ArticleCard extends LightDOMLitElement {
     render() {
         const tags = this.tags.map((tag) => html`<span class="inline-flex items-center rounded-full bg-orange-100 px-2 py-1 text-xs font-medium text-gray-600 ring-1 ring-inset ring-orange-500/10">${tag}</span>`);
         return html`
-            <div class="col-span-2 grid grid-cols-subgrid gap-4 rounded-xl border border-gray-100 bg-gradient-to-br from-blue-50 to-green-50 shadow-md hover:shadow-lg overflow-hidden">
+            <div class="col-span-2 grid grid-cols-subgrid gap-4 overflow-hidden rounded-xl border border-gray-100 bg-gradient-to-br from-blue-50 to-green-50 shadow-md hover:shadow-lg">
                 <div class="flex flex-col justify-between p-4 xl:p-6">
                     <div>
                         <h3 class="lg:text-lg">${this.title}</h3>
                         <p class="mt-4 line-clamp-3 text-sm text-gray-600 lg:text-base">${this.description}</p>
                     </div>
-                    <div class="flex flex-col gap-3 mt-4">
+                    <div class="mt-4 flex flex-col gap-3">
                         <div class="text-sm text-gray-500">
-                            <p>By <span class="font-medium text-gray-700">${this.author}</span>  <span class="text-gray-400">•</span> ${this.date}</p>
+                            <p>By <span class="font-medium text-gray-700">${this.author}</span> <span class="text-gray-400">•</span> ${this.date}</p>
                         </div>
                         <div class="flex flex-wrap gap-2">${tags}</div>
                     </div>
