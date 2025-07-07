fe5a22e4aa439374b9ea84630ecdd20d51be8552
Issack John <issackjohn@microsoft.com>
2025-07-07 10:48:04 -0700

    using matchMedia for elipses

--- experimental/responsive-design/tailwind.config.js
+++ experimental/responsive-design/tailwind.config.js
@@ -2,11 +2,7 @@
 export default {
     content: ["./src/**/*.{html,js}", "!./src/lib/components/chat-window.js", "!./src/lib/components/information-window.js", "!./src/lib/components/restaurant-card.js"],
     theme: {
-        extend: {
-            screens: {
-                "3xl": "1720px",
-            },
-        },
+        extend: {},
     },
     plugins: [],
 };
