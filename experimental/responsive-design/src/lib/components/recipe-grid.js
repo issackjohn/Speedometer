0a5b4f2c8cffd7298d63d9945bca9d7a4fb40034
Issack John <issackjohn@microsoft.com>
2025-07-15 15:27:35 -0700

    formatting

--- experimental/responsive-design/src/lib/components/recipe-grid.js
+++ experimental/responsive-design/src/lib/components/recipe-grid.js
@@ -34,17 +34,15 @@ class RecipeGrid extends LightDOMLitElement {
     }
 
     _getRecipeCardsTemplate() {
-        return this.recipes.map(
-            (recipe, index) => {
-                const isExpanded = this._expandedCardIndices.includes(index);
-                const gridClasses = this.isCompactLayout
-                    ? "relative rounded-lg bg-white text-left shadow-md hover:shadow-lg"
-                    : `${isExpanded ? "col-span-2 lg:col-span-2" : ""} grid-rows-subgrid relative row-span-6 mt-1 grid gap-0 rounded-lg bg-white text-left shadow-md hover:shadow-lg`;
-                return html` <recipe-card class="${gridClasses}" .recipe="${recipe}" .isExpanded="${isExpanded}" .isCompactMode="${this.isCompactLayout}" .index="${index}" @toggle-expand="${this._handleToggleExpand}"></recipe-card> `;
-            }
-        );
+        return this.recipes.map((recipe, index) => {
+            const isExpanded = this._expandedCardIndices.includes(index);
+            const gridClasses = this.isCompactLayout
+                ? "relative rounded-lg bg-white text-left shadow-md hover:shadow-lg"
+                : `${isExpanded ? "col-span-2 lg:col-span-2" : ""} grid-rows-subgrid relative row-span-6 mt-1 grid gap-0 rounded-lg bg-white text-left shadow-md hover:shadow-lg`;
+            return html` <recipe-card class="${gridClasses}" .recipe="${recipe}" .isExpanded="${isExpanded}" .isCompactMode="${this.isCompactLayout}" .index="${index}" @toggle-expand="${this._handleToggleExpand}"></recipe-card> `;
+        });
     }
-    
+
     render() {
         const containerClass = this.isCompactLayout ? "grid grid-cols-1 gap-3 py-2 pl-2 pr-4" : "grid grid-cols-2 gap-5 py-2 pl-2 pr-4 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 xl:gap-6";
         return html` <div class="${containerClass}" @toggle-expand="${this._handleToggleExpand}">${this._getRecipeCardsTemplate()}</div> `;
